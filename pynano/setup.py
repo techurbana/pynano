693d7bba658a591ff82d38c07b1110ba3947dddc
techurbana <techurbana@users.noreply.github.com>
2020-01-01 07:54:04 +0100

    Create setup.py

---
+++ pynano/setup.py
@@ -0,0 +1,33 @@
+"""Setup for the pynano package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Shay Palachy",
+    author_email="shay.palachy@gmail.com",
+    name='chocobo',
+    license="MIT",
+    description='chocobo is a python package for delicious chocobo recipes.',
+    version='v0.0.3',
+    long_description=README,
+    url='https://github.com/shaypal5/chocobo',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
